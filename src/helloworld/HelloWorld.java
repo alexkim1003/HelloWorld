1f0ab8f5d97167577b02fe97b45be750ede30781
Another Park <jbpark103@hanmail.net>
2016-04-07 13:16:14 +0900

    Wonderful J

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,6 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello OsParking");
+        System.out.println("Hello J");
     }
 }
