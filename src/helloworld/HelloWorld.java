5812e156131c6e0ede5b7800348dd29a6589e262
Park, Jongbum <jbpark03@gmail.com>
2016-04-07 11:04:02 +0900

    ½ǽÀ¿ë-pull request
    2016³â 4¿ù 8ÀÏ ¼¼¹̳ª¿¡¼­ Âü¼®ÀڵéÀÌ °£´ÜÇÑ pull request ½ÇÇèÀ» À§Çؼ­ ¸¸µç ÇÁ·ÎÁ§Ʈ. ù Ŀ¹Ô.

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Open Source Parking Inc.
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+}
