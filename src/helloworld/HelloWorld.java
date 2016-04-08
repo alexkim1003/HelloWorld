83f8b0004fbfe3f9f433eef85033bef1fa6425e8
alexkim1003 <alex1003@naver.com>
2016-04-08 18:53:05 +0900

    Update HelloWorld.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,6 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("request accepted테스트");
+        System.out.println("request accepted 테스트");
     }
 }
